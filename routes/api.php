2f053882f579fe607446d2de79d8b1518f28db12
Mohammad <Mohammad.Hassani.AA@gmail.com>
2025-04-25 14:19:06 +0330

    add throttle:60,1 middleware

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::post('/login', [\App\Http\Controllers\Auth\AuthController::class, 'login']);
 
-Route::middleware('JwtMiddleware')->get('/test', function () {
-    return "test";
+
+Route::middleware('throttle:60,1')->group(function () {
+    Route::post('/login', [\App\Http\Controllers\Auth\AuthController::class, 'login']);
+
+    Route::middleware('JwtMiddleware')->group(function () {
+        Route::get('/test', function () {
+            return "test";
+        });
+    });
 });
