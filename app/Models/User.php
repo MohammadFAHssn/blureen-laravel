3642cf44135f1f911a54bbddca6e523df05b86ef
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-05-01 12:29:48 +0330

    some change

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,6 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $fillable = [
         'name',
-        // 'email',
         'user_name',
         'password',
     ];
