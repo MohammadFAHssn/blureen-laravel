0e2b4360c9687adffb8edeced5ded4227c5bf9a7
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-05-27 12:59:42 +0330

    reorder test route placement within middleware for improved structure

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,13 @@ Route::middleware('throttle:60,1')->group(function () {
     Route::post('/login', [\App\Http\Controllers\Auth\AuthController::class, 'login']);
 
     Route::middleware('JwtMiddleware')->group(function () {
-        Route::get('/test', function () {
-            return "test";
-        });
 
         Route::controller(\App\Http\Controllers\UserController::class)->prefix('/base/user')->group(function () {
             Route::get('/get', 'get')->middleware(['permission:read User', 'CheckPermissionsForParamsOfUserRoute']);
         });
+    });
+
+    Route::get('/test', function () {
+        return "test";
     });
 });
