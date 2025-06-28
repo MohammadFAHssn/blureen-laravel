5a97be0b1efd2afeaf6774bd890c66f54908e369
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-06-28 13:37:17 +0330

    feat: add daily scheduled job for SyncWithRayvarz in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Jobs\SyncWithRayvarz;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -29,4 +31,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->job(new SyncWithRayvarz('supplier', 'supplierId'))
+            ->daily();
+    })
+    ->create();
