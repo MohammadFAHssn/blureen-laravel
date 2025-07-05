f69c6b8a6d74480b29d8a5b3bd8bafe060463c6b
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-07-05 13:13:31 +0330

    fix: make password field nullable in users table migration

--- database/migrations/base/0001_01_01_000000_create_users_table.php
+++ database/migrations/base/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -19,7 +18,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('username')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('personnel_code')->nullable();
             // $table->string('national_code')->unique();
             $table->string('mobile_number')->nullable();
