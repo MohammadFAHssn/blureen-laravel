eaef7466f907acc0007a25aa6c4e84546a2386b1
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-07-21 16:57:57 +0330

    fix: update fillable attributes in User model to include first_name, last_name, personnel_code, and mobile_number

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,11 @@ class User extends Authenticatable implements JWTSubject
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'username',
-        'password',
+        'personnel_code',
+        'mobile_number'
     ];
 
     /**
