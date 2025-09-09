4a0064eb4f032a20655970882c8c5b9229b27f18
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-09-09 14:24:05 +0330

    feat: add base models for CostCenter, EducationLevel, JobPosition, WorkArea, and Workplace; enhance UserProfile with relationships

--- app/Models/Base/UserProfile.php
+++ app/Models/Base/UserProfile.php
@@ -1,9 +1,32 @@
 <?php
-
 namespace App\Models\Base;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UserProfile extends Model
 {
+    public function educationLevel()
+    {
+        return $this->belongsTo(EducationLevel::class, 'education_level_id', 'rayvarz_id');
+    }
+
+    public function workplace()
+    {
+        return $this->belongsTo(Workplace::class, 'workplace_id', 'rayvarz_id');
+    }
+
+    public function workArea()
+    {
+        return $this->belongsTo(WorkArea::class, 'work_area_id', 'rayvarz_id');
+    }
+
+    public function costCenter()
+    {
+        return $this->belongsTo(CostCenter::class, 'cost_center_id', 'rayvarz_id');
+    }
+
+    public function jobPosition()
+    {
+        return $this->belongsTo(JobPosition::class, 'job_position_id', 'rayvarz_id');
+    }
 }
