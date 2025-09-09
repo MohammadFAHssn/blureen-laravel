721cf3c3aa2213c4a8826ffb9c1dbe5f1b17e6f2
mahdi <mahdi.zare.mehrjardi75@gmail.com>
2025-09-09 12:48:52 +0330

    personnelRecordController

--- app/Services/PersonnelRecords/PersonnelRecordsService.php
+++ app/Services/PersonnelRecords/PersonnelRecordsService.php
@@ -20,41 +20,41 @@ class PersonnelRecordsService
         $personnelCode = $data['personnel_code'];
         $user = User::where('personnel_code',$personnelCode)->first();
         if(is_null($user))
-            throw new CustomException('پرسنل یافت نشد.',404);
+            throw new CustomException('پرسنل یافت نشد.');
 
         $personnel_records = [];
 
         //education data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.legacy_integrated_system.get_completed_onboarding_courses'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه جامع', 500);
+        $response ? $personnel_records['education_data'] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه جامع', 500);
 
         //introduction data
-        $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.legacy_integrated_system.get_introductionToUnit_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه جامع', 500);
+        /*$response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.legacy_integrated_system.get_introductionToUnit_data'));
+        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه جامع', 500);*/
 
         //reassignment data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.legacy_integrated_system.get_reassignment_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه جامع', 500);
+        $response!==false ? $personnel_records['reassignment_data'] = $response:throw new CustomException('2خطا هنگام دریافت اطلاعات از سامانه جامع', 500);
 
         //RewardFine data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.productivity_system.get_reward_and_fines_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه بهره وری', 500);
+        $response!==false ? $personnel_records['productivity_data'] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه بهره وری', 500);
 
         //assessment data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.payroll_system.get_assessment_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه فیش حقوقی', 500);
+        $response!==false ? $personnel_records['assessment_data'] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه فیش حقوقی', 500);
 
         //salary data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.payroll_system.get_payroll_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه فیش حقوقی', 500);
+        $response!==false ? $personnel_records['payroll_data'] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه فیش حقوقی', 500);
 
         //birthday gift data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.payroll_system.get_birthday_gift_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه فیش حقوقی', 500);
+        $response!==false ? $personnel_records['birthday_gift_data'] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه فیش حقوقی', 500);
 
         //food reservation data
         $response = $this->fetchPersonnelRecordsFromSystems($personnelCode,config('services.food_reservation_system.get_food_reservation_data'));
-        $response ? $personnel_records[] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه رزرو غذا', 500);
+        $response!==false ? $personnel_records['food_reserve_data'] = $response:throw new CustomException('خطا هنگام دریافت اطلاعات از سامانه رزرو غذا', 500);
 
         return $personnel_records;
 
@@ -76,7 +76,9 @@ class PersonnelRecordsService
         if ($response->failed()) {
             return false;
         }
-        else
+        else{
             return $response->json()['data'];
+        }
+
     }
 }
