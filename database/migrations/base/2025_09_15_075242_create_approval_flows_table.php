7037529e62583b2ddabed9a66f5ba0b33c5a15a1
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-09-16 12:06:50 +0330

    refactor: remove unused morphs column from approval_flows migration

--- database/migrations/base/2025_09_15_075242_create_approval_flows_table.php
+++ database/migrations/base/2025_09_15_075242_create_approval_flows_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,8 +13,6 @@ return new class extends Migration {
     {
         Schema::create('approval_flows', function (Blueprint $table) {
             $table->id();
-
-            $table->morphs('approvable');
 
             $table->unsignedBigInteger('requester_user_id')->nullable();
             $table->foreign('requester_user_id')
@@ -32,7 +31,6 @@ return new class extends Migration {
                 ->references('id')
                 ->on('users')
                 ->restrictOnDelete();
-
 
             $table->unsignedBigInteger('approver_position_id')->nullable();
             $table->foreign('approver_position_id')
