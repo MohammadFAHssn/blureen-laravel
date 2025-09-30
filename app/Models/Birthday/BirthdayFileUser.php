6e7d866003c57626e165ae0ee5665926c2668e6e
amir karimi <amir.hssn.karimi@gmail.com>
2025-09-30 12:15:04 +0330

    add BirthdayFileUser Model

---
+++ app/Models/Birthday/BirthdayFileUser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Birthday;
+
+use App\Models\User;
+use App\Models\Birthday\BirthdayFile;
+use App\Models\Birthday\BirthdayGift;
+use Illuminate\Database\Eloquent\Model;
+
+class BirthdayFileUser extends Model
+{
+    protected $fillable = [
+        'birthday_file_id',
+        'user_id',
+        'birthday_gift_id',
+        'status',
+        'created_by',
+        'edited_by',
+    ];
+
+    public function birthdayFile()
+    {
+        return $this->belongsTo(BirthdayFile::class, 'birthday_file_id');
+    }
+
+    public function birthdayGift()
+    {
+        return $this->belongsTo(BirthdayGift::class, 'birthday_gift_id');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function editedBy()
+    {
+        return $this->belongsTo(User::class, 'edited_by');
+    }
+}
