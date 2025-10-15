71cd9d0c868e9e3630de7e1903beab6e0e305d10
amir karimi <amir.hssn.karimi@gmail.com>
2025-10-15 08:10:43 +0330

    Add health certificate models and migratoins: early test

---
+++ app/Models/HSE/HealthCertificate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\HSE;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+class HealthCertificate extends Model
+{
+    protected $fillable = [
+        'file_name',
+        'month',
+        'year',
+        'status',
+        'uploaded_by',
+        'edited_by',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(HealthCertificate::class, 'health_certificate_id');
+    }
+
+    public function uploadedBy()
+    {
+        return $this->belongsTo(User::class, 'uploaded_by');
+    }
+
+    public function editedBy()
+    {
+        return $this->belongsTo(User::class, 'edited_by');
+    }
+}
---
+++ app/Models/HSE/HealthCertificateUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\HSE;
+
+use App\Models\User;
+use App\Models\HSE\HealthCertificate;
+use Illuminate\Database\Eloquent\Model;
+
+class HealthCertificateUser extends Model
+{
+    protected $fillable = [
+        'health_certificate_id',
+        'user_id',
+        'image',
+        'status',
+        'uploaded_by',
+        'edited_by',
+    ];
+
+    public function healthCertificate()
+    {
+        return $this->belongsTo(HealthCertificate::class, 'birthday_file_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function uploadedBy()
+    {
+        return $this->belongsTo(User::class, 'uploaded_by');
+    }
+
+    public function editedBy()
+    {
+        return $this->belongsTo(User::class, 'edited_by');
+    }
+}
