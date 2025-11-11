fb174cf6a6d9e3695e0756a012629749959dceb5
amir karimi <amir.hssn.karimi@gmail.com>
2025-11-11 09:23:06 +0330

    update HealthCertificateController: add index, update, delete and show methods.

--- app/Http/Controllers/HSE/HealthCertificateController.php
+++ app/Http/Controllers/HSE/HealthCertificateController.php
@@ -10,6 +10,7 @@ use App\Services\HSE\HealthCertificateService;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Requests\HSE\CreateHealthCertificateRequest;
+use App\Http\Requests\HSE\UpdateHealthCertificateRequest;
 
 /**
  * Class HealthCertificateController
@@ -60,6 +61,144 @@ class HealthCertificateController
                 'message' => 'اطلاعات وارد شده معتبر نیست.',
                 'status' => 422,
                 'code' => 'VALIDATION_ERROR',
+            ];
+
+            return response()->json($payload, $payload['status']);
+        } catch (Throwable $e) {
+            $payload = [
+                'error' => $e->getMessage(),
+                'status' => 500,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        }
+    }
+
+    /**
+     * Get all HealthCertificate Periods
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        try {
+            $data = $this->healthCertificateService->getAllHealthCertificates();
+
+            $payload = [
+                'data' => $data,
+                'message' => 'لیست دوره‌های شناسنامه سلامت با موفقیت دریافت شد.',
+                'status' => 200,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        } catch (Throwable $e) {
+            $payload = [
+                'error' => $e->getMessage(),
+                'status' => 500,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        }
+    }
+
+    /**
+     * Update a HealthCertificate
+     *
+     * @param UpdateHealthCertificateRequest $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(UpdateHealthCertificateRequest $request, int $id)
+    {
+        try {
+            $data = $this->healthCertificateService->updateHealthCertificate($id, $request->validated());
+
+            $payload = [
+                'data' => $data,
+                'message' => 'شناسنامه سلامت با موفقیت بروزرسانی شد.',
+                'status' => 200,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        } catch (ModelNotFoundException $e) {
+            $payload = [
+                'message' => 'شناسنامه سلامت مورد نظر یافت نشد.',
+                'status' => 404,
+                'code' => 'HEALTH_CERTIFICATE_NOT_FOUND',
+            ];
+
+            return response()->json($payload)->setStatusCode($payload['status']);
+        } catch (ValidationException $e) {
+            $payload = [
+                'errors' => $e->errors(),
+                'message' => 'اطلاعات وارد شده معتبر نیست.',
+                'status' => 422,
+                'code' => 'VALIDATION_ERROR',
+            ];
+
+            return response()->json($payload, $payload['status']);
+        } catch (Throwable $e) {
+            $payload = [
+                'error' => $e->getMessage(),
+                'status' => 500,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        }
+    }
+
+    /**
+     * Delete HealthCertificate
+     *
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id)
+    {
+        try {
+            $data = $this->healthCertificateService->delete($id);
+
+            $payload = [
+                'data' => $data,
+                'message' => 'شناسنامه سلامت با موفقیت حذف شد.',
+                'status' => 200,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        } catch (ModelNotFoundException $e) {
+            $payload = [
+                'message' => 'شناسنامه سلامت مورد نظر یافت نشد.',
+                'status' => 404,
+                'code' => 'HEALTH_CERTIFICATE_NOT_FOUND',
+            ];
+
+            return response()->json($payload)->setStatusCode($payload['status']);
+        } catch (Throwable $e) {
+            $payload = [
+                'error' => $e->getMessage(),
+                'status' => 500,
+            ];
+
+            return response()->json($payload, $payload['status']);
+        }
+    }
+
+    /**
+     * Show a specific HealthCertificate
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id)
+    {
+        try {
+            $data = $this->healthCertificateService->getHealthCertificate($id);
+
+            $payload = [
+                'data' => $data,
+                'message' => 'شناسنامه سلامت با موفقیت دریافت شد.',
+                'status' => 200,
             ];
 
             return response()->json($payload, $payload['status']);
