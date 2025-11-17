91b887848fa2e9da3740abd3c726ec274166369f
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-11-17 15:15:12 +0330

    Update user profile migration: enforce unique constraint on user_id field

--- database/migrations/base/2025_09_08_122823_create_user_profiles_table.php
+++ database/migrations/base/2025_09_08_122823_create_user_profiles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade');
 
             $table->string('national_code')->nullable()->unique();
             $table->string('gender')->nullable();
