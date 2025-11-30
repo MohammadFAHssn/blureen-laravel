02b0bdf3588426c0584c3e06c85a80b1f5344931
amir karimi <amir.hssn.karimi@gmail.com>
2025-11-30 13:55:07 +0330

    add Meal Migratoin

--- database/migrations/food/2025_11_30_135154_create_meals_table.php
+++ database/migrations/food/2025_11_30_135154_create_meals_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('meels', function (Blueprint $table) {
+        Schema::create('meals', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->boolean('status')->default(1);
@@ -25,6 +26,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('meels');
+        Schema::dropIfExists('meals');
     }
 };
