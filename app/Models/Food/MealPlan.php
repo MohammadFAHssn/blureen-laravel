8088e35843656c99f4dfb0e10148f819967576fd
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-02 07:35:32 +0330

    add MealPlan Model and Migration

---
+++ app/Models/Food/MealPlan.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models\Food;
+
+use App\Models\Food\Food;
+use App\Models\Food\Meal;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+class MealPlan extends Model
+{
+    protected $table = 'meal_plans';
+
+    protected $fillable = [
+        'year',
+        'month',
+        'day',
+        'meal_id',
+        'food_id',
+        'created_by',
+        'edited_by',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function editedBy()
+    {
+        return $this->belongsTo(User::class, 'edited_by');
+    }
+
+    public function meal()
+    {
+        return $this->belongsTo(Meal::class, 'meal_id');
+    }
+
+    public function food()
+    {
+        return $this->belongsTo(Food::class, 'food_id');
+    }
+}
---
+++ database/migrations/food/2025_12_01_102750_create_meal_plans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meal_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('year');
+            $table->string('month');
+            $table->string('day');
+            $table->unsignedInteger('meal_id');
+            $table->unsignedInteger('food_id');
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('edited_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meal_plans');
+    }
+};
