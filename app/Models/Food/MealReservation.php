909512b320a2d6b5ebcfd23bb8340d4ad08fc72b
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-03 16:32:08 +0330

    add MealReservation Model and Migration

---
+++ app/Models/Food/MealReservation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models\Food;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+class MealReservation extends Model
+{
+    protected $table = 'meal_reservations';
+
+    protected $fillable = [
+        'date',
+        'meal_id',
+        'reserve_type',  // personnel, contractor or guest
+        'supervisor_id',  // created_by for now, may change in future - in future use, this means personnel x reserved food for staff, contractor or guest related to personnel y
+        'delivery_code',
+        'description',  // mandatory only for guest
+        'created_by',
+        'edited_by',
+    ];
+
+    protected $casts = [
+        'date'          => 'date',
+        'meal_id'       => 'integer',
+        'reserve_type'  => 'string',
+        'supervisor_id' => 'integer',
+        'delivery_code' => 'integer',
+        'created_by'    => 'integer',
+        'edited_by'     => 'integer',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function editedBy()
+    {
+        return $this->belongsTo(User::class, 'edited_by');
+    }
+
+    public function meal()
+    {
+        return $this->belongsTo(Meal::class, 'meal_id');
+    }
+
+    public function supervisor()
+    {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
+    public function details()
+    {
+        return $this->hasMany(MealReservationDetail::class, 'meal_reservation_id');
+    }
+}
---
+++ database/migrations/food/2025_12_03_113606_create_meal_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meal_reservations', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->foreignId('meal_id')->constrained('meals');
+            $table->string('reserve_type');  // 'personnel', 'contractor', 'guest'
+            $table->foreignId('supervisor_id')->constrained('users');
+            $table->unsignedBigInteger('delivery_code')->unique();
+            $table->text('description')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('edited_by')->nullable()->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meal_reservations');
+    }
+};
