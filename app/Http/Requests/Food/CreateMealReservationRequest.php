74f76932f1d0d5f19d5a707b59dea9db6f016354
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-09 13:33:06 +0330

    update CreateMealReservationRequest to receive all informations for all types of meal reservations

--- app/Http/Requests/Food/CreateMealReservationRequest.php
+++ app/Http/Requests/Food/CreateMealReservationRequest.php
@@ -36,7 +36,7 @@ class CreateMealReservationRequest extends FormRequest
             'personnel.*' => 'required_if:reserve_type,personnel|integer|exists:users,id',
 
             // contractor
-            'contractor' => 'required_if:reserve_type,contractor',
+            'contractor' => 'required_if:reserve_type,contractor|in:contractors,id',
 
             // quantity (both contractor & guest)
             'quantity' => 'required_if:reserve_type,contractor,guest|integer|min:1',
