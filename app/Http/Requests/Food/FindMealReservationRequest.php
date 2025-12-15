56543df5c46f81c8f489a0cf13f3e72eaa5d7929
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-15 12:39:57 +0330

    update FindMealReservationRequest

--- app/Http/Requests/Food/FindMealReservationRequest.php
+++ app/Http/Requests/Food/FindMealReservationRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Food;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class FindMealReservationRequest extends FormRequest
 {
@@ -25,5 +27,30 @@ class FindMealReservationRequest extends FormRequest
             'date' => 'required|date',  // e.g. Y-m-d
             'delivery_code' => 'required|integer|exists:meal_reservations,delivery_code',
         ];
+    }
+
+    /**
+     * Custom validation messages
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'date.required' => 'تاریخ الزامی است.',
+            'date.date' => 'فرمت تاریخ معتبر نیست.',
+            'delivery_code.required' => 'کد تحویل الزامی است.',
+            'delivery_code.integer' => 'کد تحویل باید عدد باشد.',
+            'delivery_code.exists' => 'کد تحویل معتبر نیست.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'خطا در اعتبارسنجی اطلاعات ورودی!',
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
