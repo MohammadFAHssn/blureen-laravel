d3350a77b07da26d70072d43c8302e4a5ee9dbd1
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-18 12:17:53 +0330

    update  UserRepository: Get user by ID.

--- app/Repositories/Base/UserRepository.php
+++ app/Repositories/Base/UserRepository.php
@@ -33,4 +33,16 @@ class UserRepository
                 'approvalFlowsAsRequester.approverPosition:rayvarz_id,name',
             ])->get();
     }
+
+    /**
+     * Get user by ID
+     *
+     * @param int $id
+     * @return User
+     * @throws ModelNotFoundException
+     */
+    public function findById(int $id): User
+    {
+        return User::findOrFail($id);
+    }
 }
