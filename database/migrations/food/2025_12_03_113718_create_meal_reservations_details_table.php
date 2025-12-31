0b94c7e2982dc119cb8c03c930f2509bc27c1255
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-31 09:59:53 +0330

    update meal_reservations_details migration

--- database/migrations/food/2025_12_03_113718_create_meal_reservations_details_table.php
+++ database/migrations/food/2025_12_03_113718_create_meal_reservations_details_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->boolean('delivery_status')->default(0);
             $table->time('check_out_time')->nullable();
             $table->date('last_check_at')->nullable();
-            $table->boolean('is_entitled')->default(0);
+            $table->boolean('is_entitled')->nullable();
             $table->foreignId('created_by')->constrained('users');
             $table->foreignId('edited_by')->nullable()->constrained('users');
             $table->timestamps();
