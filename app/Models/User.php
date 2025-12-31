0c2c904d648733b5a883974689741dafbb58e577
amir karimi <amir.hssn.karimi@gmail.com>
2025-12-31 11:57:04 +0330

    Remove unused import for BelongsTo relation in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ use App\Models\HSE\HealthCertificateUser;
 use App\Models\HrRequest\HrRequest;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
